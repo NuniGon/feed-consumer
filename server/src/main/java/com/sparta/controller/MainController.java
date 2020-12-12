3941095773f2f519d43a33fd62931db3b930f4eb
nunigs <nuni.gs@gmail.com>
2020-12-12 18:04:38 +0000

    Add pom.xml

--- server/src/main/java/com/sparta/controller/MainController.java
+++ server/src/main/java/com/sparta/controller/MainController.java
@@ -1,20 +1,25 @@
 package com.sparta.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class MainController {
 
   @PostMapping("/load/{provider}")
-  public int load(@PathVariable("provider") String provider, @RequestBody byte[] content) throws IOException {
+  public ResponseEntity<String> load(@PathVariable("provider") String provider, @RequestBody byte[] content) throws IOException {
+    return ResponseEntity.ok("hola");
   }
 
   @GetMapping("/data/{provider}/total")
-  public int total(@PathVariable("provider") String provider) {
+  public ResponseEntity<String> total(@PathVariable("provider") String provider) {
+    return ResponseEntity.ok("hola");
   }
 
 }
